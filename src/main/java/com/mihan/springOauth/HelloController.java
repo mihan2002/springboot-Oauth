8b77c79710c3a1ec0e0e566c4b6490339ce0f1f8
mihan2002 <mihanamajith20@gmail.com>
2025-04-09 14:49:31 +0530

    initail commit

---
+++ src/main/java/com/mihan/springOauth/HelloController.java
@@ -0,0 +1,13 @@
+package com.mihan.springOauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String Hello(){
+        return "Hello";
+    }
+}
